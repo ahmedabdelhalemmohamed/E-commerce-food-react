f0aa4ec14edbbadebccf3e47475f81ac00560eb2
ahmedabdelhalemmohamed <ahmedabdelhalemmohamed2026@gmail.com>
2026-03-30 20:33:31 +0200

    add data for projects

--- src/js/Products.js
+++ src/js/Products.js
@@ -1,21 +1,75 @@
 const products = [
     {
-        name: "Eggs",
-        price: 2.99,
-        image: "https://cdn.pixabay.com/photo/2017/01/20/00/30/eggs-1995056_1280.jpg",
-        category: "Healthy"
-    },
-    {
-        name: "Eggs",
-        price: 2.99,
-        image: "https://cdn.pixabay.com/photo/2017/01/20/00/30/eggs-1995056_1280.jpg",
-        category: "Healthy"
-    },
-    {
-        name: "fast Food",
-        price: 2.99,
-        image: "https://cdn.pixabay.com/photo/2017/01/20/00/30/eggs-1995056_1280.jpg",
+        name: "Burger",
+        price: 3,
+        image: "./foods/fastFood/burger/burger1.jpg",
         category: "Fast Food"
+    },
+    {
+        name: "Burger",
+        price: 4,
+        image: "./foods/fastFood/burger/burger2.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Hot Dog",
+        price: 4,
+        image: "./foods/fastFood/hotDog/hotDog1.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Hot Dog",
+        price: 5,
+        image: "./foods/fastFood/hotDog/hotDog2.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Shanarma",
+        price: 5,
+        image: "./foods/fastFood/shawarma/shawarma1.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Shanarma",
+        price: 6,
+        image: "./foods/fastFood/shawarma/shawarma2.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Chicken Wrap",
+        price: 6,
+        image: "./foods/fastFood/chickenWrap/chickenWrap1.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Chicken Wrap",
+        price: 7,
+        image: "./foods/fastFood/chickenWrap/chickenWrap2.jpg",
+        category: "Fast Food"
+    },
+    {
+        name: "Spaghetti",
+        price: 3,
+        image: "./foods/fastFood/spaghetti/spaghetti1.jpg",
+        category: "Italian"
+    },
+    {
+        name: "Spaghetti",
+        price: 4,
+        image: "./foods/fastFood/spaghetti/spaghetti2.jpg",
+        category: "Italian"
+    },
+    {
+        name: "Penne",
+        price: 4,
+        image: "./foods/fastFood/penne/penne1.jpg",
+        category: "Italian"
+    },
+    {
+        name: "Penne",
+        price: 5,
+        image: "./foods/fastFood/penne/penne2.jpg",
+        category: "Italian"
     },
 ];
 
